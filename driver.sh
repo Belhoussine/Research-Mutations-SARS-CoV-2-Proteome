56cdcb86d4c98f051e8fb9a352a93e164732cb59
GideonWolfe <wolfeg2@wwu.edu>
2020-05-22 12:19:59 -0700

    driver that tidies each mutation into a subdirectory with EM data

--- driver.sh
+++ driver.sh
@@ -4,26 +4,51 @@
 # Change the "FILES" variable to the script (or scripts) you would like to execute
 # to profile different mutation scripts, you can run this script with the "time" command 
 
-# generate two promute scripts
-./makeScript 1hhp A:A 1:30 X emScript
-./makeScript 1hhp A:A 1:30 X emsrScript
+# generate list of mutations
+# ./makeScript 1hhp A:A 1:10 X testMutations
 
 # append the correct flags
-sed -i 's/$/ emsr/' emsrScript
-sed -i 's/$/ em/' emScript
+# sed -i 's/$/ em/' testMutations
 
 # Customize with your scripts
-FILES=("emScript")
+FILES=("testMutations")
 
 # Execute the lines in the script
 for FILE in $FILES; do
+
+  # Create a directory to store the output of the mutation script
+  mkdir -p "${FILE}_output"
+
+  # Read each mutation into an array
   readarray -t LINES < "$FILE"
+
+  # Routine to execute for every mutation
   for LINE in "${LINES[@]}"; do
+
+    # Make a directory to store outputs
+    # Structure is PDBID.Res1.Location.Res2.output
+    OUTPUTDIRNAME=$(echo $LINE | awk '{ print $2"."$3"."$4"."$5".output" }')
+    echo [INFO] Creating Directory $OUTPUTDIRNAME
+    mkdir -p $OUTPUTDIRNAME
+
+    # Directory to look for EM files that were generated
+    # Structure is PDBID.Res1LocationRes2
+    EMDIRNAME=$(echo $LINE | awk '{ print $2"."$3$4$5 }')
+
     $LINE
+
+    # Move all output files: PDB, Fasta, and EM data
+    mv *.txt $OUTPUTDIRNAME
+    mv *.pdb $OUTPUTDIRNAME
+    mv external/em/$EMDIRNAME $OUTPUTDIRNAME
+    echo [INFO] Moving files to $OUTPUTDIRNAME
+
   done
   # cleanup output
-  mkdir -p "${FILE}_output"
-  mv *.txt "${FILE}_output"
-  mv *.pdb "${FILE}_output"
+  rm -rf "${FILE}_output"
+  echo [INFO] Cleaning outputs from last run
+  echo [INFO] Moving new files to "${FILE}_output"
+  mv *.output "${FILE}_output"
+
 done
 
