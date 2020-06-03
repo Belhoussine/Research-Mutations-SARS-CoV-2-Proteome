07c1cf39b3244b3e159ba82cdaccb63cc72808aa
Gideon Howard Wolfe <wolfeg2@linux-11.cs.wwu.edu>
2020-06-03 12:32:55 -0700

    data for run of 6 residues

---
+++ data/test2/Y453_output/6M0J.E.453.Q.output/6M0J.E453Q/newpsf.tcl
@@ -0,0 +1,24 @@
+package require psfgen
+
+resetpsf
+
+topology minimization/scripts/top_all27_prot_lipid_na.inp
+
+pdbalias residue HIS HSD
+pdbalias residue CYX CYS
+pdbalias atom ILE CD1 CD
+
+segment A {
+
+first NTER
+last CTER
+pdb chainA.pdb
+
+}
+coordpdb chainA.pdb A
+guesscoord
+
+writepsf 6M0J.E453Q_autopsf.psf
+
+writepdb 6M0J.E453Q_autopsf.pdb
+
